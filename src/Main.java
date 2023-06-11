ea379644a064cbd0ef2dc00991c48d1d58bd7022
Kidongg <dlrlehd7@daum.net>
2023-06-11 17:27:32 +0900

    Java

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+// 클래스
+public class Main {
+    // 메인 메서드
+    // JVM은 메인 메소드를 제일 먼저 실행시킨다.
+    public static void main(String[] args) {
+        System.out.println("Our First Project :)");
+
+        System.out.println(7);
+        System.out.println(3);
+        System.out.println(3.14);
+        System.out.println("JAVA");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
