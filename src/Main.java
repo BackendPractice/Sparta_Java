d8d4197ad03141534b89f4b558d5841ea0b4b632
Kidongg <dlrlehd7@daum.net>
2023-06-12 12:26:05 +0900

    data type

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,39 @@
-// 클래스
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
-    // 메인 메서드
-    // JVM은 메인 메소드를 제일 먼저 실행시킨다.
     public static void main(String[] args) {
-        System.out.println("Our First Project :)");
+        // 변수 선언
+        final boolean isClickBtn = false;
 
-        System.out.println(7);
-        System.out.println(3);
-        System.out.println(3.14);
-        System.out.println("JAVA");
+        char alphavbet = 'A';  // 문자형은 작은따옴표('')로 할당 해야한다.
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        byte byteNum = 127;  // 1byte
+        short shortNum = 32767; // 2byte
+        int intNum = 2147483647;  // 4byte
+        long longNum = 2147483647L;  // 8byte
+
+        float floatNum = 2.2F;  // 4byte
+        double doubleNum = 2.2;  // 8byte
+
+        System.out.println(isClickBtn);
+        System.out.println(alphavbet);
+        System.out.println(byteNum);
+        System.out.println(shortNum);
+        System.out.println(intNum);
+        System.out.println(longNum);
+        System.out.println(floatNum);
+        System.out.println(doubleNum);
+
+        String sayHello = "Hello JAVA";  // 문자열
+        int[] intArray = {1,2,3};  // 배열
+        System.out.println(sayHello);
+        System.out.println(intArray);  // 주소값 출력
+        System.out.println(Arrays.toString((intArray)));  // 주소값을 배열로 출력
+
+        // 래퍼 클래스
+        int myAge = 28;
+        Integer age = myAge;  // 박싱
+        System.out.println(age.intValue());  // 언박싱
     }
 }
