12ca43d30d3ee5928ba3475b6f4ec20607ce8af7
Kidongg <dlrlehd7@daum.net>
2023-06-13 17:52:10 +0900

    type transformations

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // 암시적 형변환 : 작은 타입의 크기에서 큰 타입의 크기로 이동할때 자동으로 타입 변환이 일어난다.
-        // 변수 타입별 크기 순서 : byte(1) => short(2) -> int(4) -> long(8) -> float(4) -> double(8)
+        int intNum = 10;
+        double doubleNum = 5.5;
 
-        // (1) byte -> int
-        byte byteNum = 10;
-        int intNum = byteNum;
-        System.out.println("intNum: "+ intNum);
+        System.out.println(intNum + doubleNum);
 
-        // (2) char -> int
-        char charAlphabat = 'A';
-        intNum = charAlphabat;
-        System.out.println("intNum: "+ intNum);
+        int intResult = intNum/4;
+        System.out.println(intResult);
 
-        // (3) int -> long
-        long longNum = intNum;
-        System.out.println("longNum: "+ longNum);
-
-        // (4) int -> double
-        double doubleNum = intNum;
-        System.out.println("doubleNum: "+ doubleNum);
+        double doubleResult = intNum/4.0;
+        System.out.println(doubleResult);
     }
 }
