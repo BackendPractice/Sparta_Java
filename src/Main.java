6033e9ff66ce1400cb6be211937147bce49ddc7a
Kidongg <dlrlehd7@daum.net>
2023-06-13 19:50:58 +0900

    first_week_homework

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int intNum = 10;
-        double doubleNum = 5.5;
+        Scanner sc = new Scanner(System.in);  // Scanner 클래스 생성
 
-        System.out.println(intNum + doubleNum);
+        // 입력값 받기
+        String title = sc.nextLine();
+        float floatRating = sc.nextFloat();
+        sc.nextLine();  // 개행 문자 소비
+        String recipe_1 = sc.nextLine();
+        String recipe_2 = sc.nextLine();
+        String recipe_3 = sc.nextLine();
 
-        int intResult = intNum/4;
-        System.out.println(intResult);
+        int intRating = (int)floatRating;  // 강제 형 변환
 
-        double doubleResult = intNum/4.0;
-        System.out.println(doubleResult);
+        // 출력하기
+        System.out.println("[" + title + "]");
+        System.out.println(intRating  + "(" + intRating*100/5.0 + "%)");  // 자동 형 변환
+        System.out.println("1. " + recipe_1);
+        System.out.println("2. " + recipe_2);
+        System.out.println("3. " + recipe_3);
     }
 }
