6de4d1bb7441309700cc48755294c63a4434cab7
Kidongg <dlrlehd7@daum.net>
2023-06-13 17:43:27 +0900

    Implicit transformations

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // 정수를 실수로 형 변환하는 예제
-        int intNum = 10;
+        // 암시적 형변환 : 작은 타입의 크기에서 큰 타입의 크기로 이동할때 자동으로 타입 변환이 일어난다.
+        // 변수 타입별 크기 순서 : byte(1) => short(2) -> int(4) -> long(8) -> float(4) -> double(8)
 
-        double doubleNum = (double)intNum;
-        float floatNum = (float)intNum;
+        // (1) byte -> int
+        byte byteNum = 10;
+        int intNum = byteNum;
+        System.out.println("intNum: "+ intNum);
 
-        System.out.println("intNum: " + intNum);
-        System.out.println("doubleNum: " + doubleNum);
-        System.out.println("floatNum: " + floatNum);
+        // (2) char -> int
+        char charAlphabat = 'A';
+        intNum = charAlphabat;
+        System.out.println("intNum: "+ intNum);
+
+        // (3) int -> long
+        long longNum = intNum;
+        System.out.println("longNum: "+ longNum);
+
+        // (4) int -> double
+        double doubleNum = intNum;
+        System.out.println("doubleNum: "+ doubleNum);
     }
 }
