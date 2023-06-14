6a1403511de801f0ab563caef95842fbcb1cb824
Kidongg <dlrlehd7@daum.net>
2023-06-14 09:22:45 +0900

    calculation

---
+++ src/calculation/Video_01.java
@@ -0,0 +1,19 @@
+package calculation;
+
+// 메인 클래스
+public class Video_01 {
+    // 메인 메소드
+    public static void main(String[] args) {
+        // 우선순위 연산
+        System.out.println(2 + 2 * 2);  // 6
+        System.out.println((2 + 2) * 2);  // 8
+        System.out.println("-----------------");
+
+        // 변수를 이용한 연산
+        int x = 5;
+        float y = 23.001f;
+        float z = x + y;
+
+        System.out.println("z: " + z);  // z: 28.001
+    }
+}
