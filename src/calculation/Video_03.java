b20ead4f06cd89d314fa82824d900653357162f2
Kidongg <dlrlehd7@daum.net>
2023-06-14 15:18:59 +0900

    substitution calculation

---
+++ src/calculation/Video_03.java
@@ -0,0 +1,34 @@
+package calculation;
+
+public class Video_03 {
+    public static void main(String[] args) {
+        // 대입 연산
+        int num = 10;
+
+        num = num + 2;
+        System.out.println(num);
+
+        num = num - 5;
+        System.out.println(num);
+
+        System.out.println("------------------");
+
+        // 복합 대입 연산
+        num = 10;
+
+        num += 2;
+        System.out.println(num);
+
+        num -= 5;
+        System.out.println(num);
+
+        System.out.println("------------------");
+        num = 10;
+
+        num ++;
+        System.out.println(num);
+
+        num --;
+        System.out.println(num);
+    }
+}
