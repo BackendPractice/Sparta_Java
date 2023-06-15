02fb2ae51f61faf17c0382e63fb5383e8864da54
Kidongg <dlrlehd7@daum.net>
2023-06-15 10:57:14 +0900

    if

---
+++ src/calculation/Video_07.java
@@ -0,0 +1,25 @@
+package calculation;
+
+public class Video_07 {
+    public static void main(String[] args) {
+        // if ~ else
+        boolean isFlag = false;
+
+        if (isFlag) {
+            System.out.println("if문 실행됨");
+        } else {
+            System.out.println("else문 실행됨");
+        }
+
+        // if ~ else if ~ else
+        int num = 2;
+
+        if (num == 1) {
+            System.out.println("if문 실행됨");
+        } else if (num == 2) {
+            System.out.println("else if문 실행됨");
+        } else {
+            System.out.println("else문 실행됨");
+        }
+    }
+}
---
+++ src/calculation/Video_08.java
@@ -0,0 +1,54 @@
+package calculation;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Video_08 {
+    public static void main(String[] args) {
+        // 가위바위보 프로그램
+        Scanner sc = new Scanner(System.in);
+
+        // A 입력
+        System.out.println("A를 입력해주세요");
+        String aHand = sc.nextLine();
+
+        // B 입력
+        System.out.println("B를 입력해주세요");
+        String bHand = sc.nextLine();
+
+        // Objects.equals(A, B) : 두 값을 비교하는 메소드
+        if (Objects.equals(aHand, "바위")) {
+            if(Objects.equals(bHand, "바위")) {
+                System.out.println("A와 B는 비겼습니다.");
+            } else if (Objects.equals(bHand, "가위")) {
+                System.out.println("A가 B를 이겼습니다.");
+            } else if (Objects.equals(bHand, "보")) {
+                System.out.println("B가 A를 이겼습니다.");
+            } else {
+                System.out.println("B가 가위바위보를 하지 않았습니다.");
+            }
+        } else if (Objects.equals(aHand, "가위")) {
+            if(Objects.equals(bHand, "바위")) {
+                System.out.println("B가 A를 이겼습니다.");
+            } else if (Objects.equals(bHand, "가위")) {
+                System.out.println("A와 B는 비겼습니다.");
+            } else if (Objects.equals(bHand, "보")) {
+                System.out.println("A가 B를 이겼습니다.");
+            } else {
+                System.out.println("B가 가위바위보를 하지 않았습니다.");
+            }
+        } else if (Objects.equals(aHand, "보")) {
+            if(Objects.equals(bHand, "바위")) {
+                System.out.println("A가 B를 이겼습니다.");
+            } else if (Objects.equals(bHand, "가위")) {
+                System.out.println("B가 A를 이겼습니다.");
+            } else if (Objects.equals(bHand, "보")) {
+                System.out.println("A와 B는 비겼습니다.");
+            } else {
+                System.out.println("B가 가위바위보를 하지 않았습니다.");
+            }
+        } else {
+            System.out.println("A가 가위바위보를 하지 않았습니다.");
+        }
+    }
+}
