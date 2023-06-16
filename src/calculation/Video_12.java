3a2074e183f06ff772dbef84f9f1c56784892ae8
Kidongg <dlrlehd7@daum.net>
2023-06-16 14:49:26 +0900

    multiplication table

---
+++ src/calculation/Video_12.java
@@ -0,0 +1,23 @@
+package calculation;
+
+import java.util.Scanner;
+
+public class Video_12 {
+    public static void main(String[] args) {
+        // 입력 받는 단만 출력
+        Scanner sc = new Scanner(System.in);  // Scanner 클래스 생성
+
+        System.out.println("원하는 단을 입력해주세요");
+        int num = sc.nextInt();
+
+        // 구구단
+        for (int i=2; i < 10; i++) {
+            // 입력 받은 단만 출력
+            if (i == num) {
+                for (int j=1; j<10; j++) {
+                    System.out.println("i*j = " + i*j);
+                }
+            }
+        }
+    }
+}
