df9b3393f5b63719dd682bb4811f8b33873a2229
Kidongg <dlrlehd7@daum.net>
2023-06-17 14:06:13 +0900

    copy array

---
+++ src/calculation/Video_13.java
@@ -0,0 +1,46 @@
+package calculation;
+
+import java.util.Arrays;
+
+public class Video_13 {
+    public static void main(String[] args) {
+        int[] intArray = new int[3]; // 초기값 {0,0,0}
+        boolean[] boolArray = new boolean[3]; // 초기값 {false, false, false}
+        String[] stringArray = new String[3]; // 초기값 {"","",""}
+
+        System.out.println(Arrays.toString(intArray));
+        System.out.println(Arrays.toString(boolArray));
+        System.out.println(Arrays.toString(stringArray));
+        System.out.println("----------------");
+
+        // 단건 조회
+        System.out.println(intArray[1]);
+        System.out.println("----------------");
+
+        // 다건 조회
+        for (int index = 0; index < intArray.length; index++) {
+            System.out.println(intArray[index]);
+        }
+
+        System.out.println("----------------");
+
+        // 초기화
+        // (1) {}
+        int[] intArr = {1, 2, 3, 4, 5};
+
+        System.out.println(Arrays.toString(intArr));
+        System.out.println("----------------");
+
+        // (2) for문
+        for (int i=0; i<intArr.length; i++) {
+            intArr[i] = i;
+        }
+
+        System.out.println(Arrays.toString(intArr));
+        System.out.println("----------------");
+
+        // (3) Arrays.fill : 배열의 요소를 같은 값으로 초기화
+        Arrays.fill(intArr, 1);
+        System.out.println(Arrays.toString(intArr));
+    }
+}
---
+++ src/calculation/Video_14.java
@@ -0,0 +1,45 @@
+package calculation;
+
+import java.util.Arrays;
+
+public class Video_14 {
+    public static void main(String[] args) {
+        // 얕은 복사
+        int[] intArr_1 = {1,2,3};
+        int[] intArr_2 = intArr_1;
+        System.out.println(Arrays.toString(intArr_1));  // [1, 2, 3]
+        System.out.println("-----------");
+
+        // intArr_2의 요소를 바꿨으나 intArr_1의 요소도 바뀌었다.
+        // 얕은 복사로 같은 주소값을 바라보고 있기 때문이다.
+        intArr_2[0] = 28;
+        System.out.println(Arrays.toString(intArr_1));  // [28, 2, 3]
+        System.out.println("-----------");
+
+        // 깊은 복사
+        // (1) for문
+        int[] forArr = new int[intArr_1.length];
+
+        for (int index=0; index < intArr_1.length; index++) {
+            forArr[index] = intArr_1[index];
+        }
+
+        forArr[0] = 38;
+        System.out.println(Arrays.toString(intArr_1));  // [28, 2, 3]
+        System.out.println("-----------");
+
+        // (2) clone
+        // 2차원 이상 배열에서는 얕은 복사로 동작한다.
+        int[] cloneArr = intArr_1.clone();
+
+        cloneArr[0] = 38;
+        System.out.println(Arrays.toString(intArr_1));  // [28, 2, 3]
+        System.out.println("-----------");
+
+        // (3) Arrays.copyOf(Array, Array.length)
+        int[] copyOfArr = Arrays.copyOf(intArr_1, intArr_1.length);
+
+        copyOfArr[0] = 38;
+        System.out.println(Arrays.toString(intArr_1));  // [28, 2, 3]
+    }
+}
