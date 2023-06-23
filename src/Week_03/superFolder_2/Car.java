e803b61461452dd252a323aecc2ff33a5e0af84d
Kidongg <dlrlehd7@daum.net>
2023-06-23 21:51:53 +0900

    super

--- src/Week_03/superFolder_2/Car.java
+++ src/Week_03/superFolder_2/Car.java
@@ -1,11 +1,16 @@
-package Week_03.superFolder;
-
+package Week_03.superFolder_2;
 public class Car {
     String model; // 자동차 모델
     String color; // 자동차 색상
+    double price; // 자동차 가격
 
     double speed;  // 자동차 속도 , km/h
 
+    public Car(String model, String color, double price) {
+        this.model = model;
+        this.color = color;
+        this.price = price;
+    }
 
     public String getModel() {
         return model;
@@ -13,6 +18,10 @@ public class Car {
 
     public String getColor() {
         return color;
+    }
+
+    public double getPrice() {
+        return price;
     }
 
     public double brakePedal() {
@@ -25,3 +34,4 @@ public class Car {
     }
 
 }
+
