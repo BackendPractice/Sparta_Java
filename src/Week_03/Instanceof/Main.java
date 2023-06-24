5c64a1b7b46563d0506e8d06c8e40027dfda488b
Kidongg <dlrlehd7@daum.net>
2023-06-24 18:50:14 +0900

    instanceof

---
+++ src/Week_03/Instanceof/Main.java
@@ -0,0 +1,23 @@
+package Week_03.Instanceof;
+
+// Main.java
+
+class Parent { }  // Parent.java
+class Child extends Parent { }  // Child.java
+
+public class Main {
+    public static void main(String[] args) {
+        Parent p = new Parent();
+
+        System.out.println(p instanceof Object);  // true
+        System.out.println(p instanceof Parent);  // true
+        System.out.println(p instanceof Child);  // false
+
+        Parent c = new Child();
+
+        System.out.println(c instanceof Object);  // true
+        System.out.println(c instanceof Parent);  // true
+        System.out.println(c instanceof Child);   // true
+
+    }
+}
--- src/Week_03/Polymorphism_3/Main.java
+++ src/Week_03/Polymorphism_3/Main.java
@@ -2,15 +2,19 @@ package Week_03.Polymorphism_3;
 
 public class Main {
     public static void main(String[] args) {
-        // 매개변수 다형성 확인!
-        Car car1 = new Car(new HankookTire("HANKOOK"));
-        Car car2 = new Car(new KiaTire("KIA"));
+        // 강제 형 변환
+        Tire hankookSampleTire = new HankookTire("HANKOOK");
+        Tire kiaSampleTire = new KiaTire("KIA");
+
+        // 다형성 구현
+        Car car1 = new Car(hankookSampleTire);
+        Car car2 = new Car(kiaSampleTire);
 
         // 오버라이딩된 메서드 호출
         car1.tire.rideComfort();  // HANKOOK 타이어 승차감은 100
         car2.tire.rideComfort();  // KIA 타이어 승차감은 60
 
-        // 반환타입 다형성 확인!
+        // 다형성 구현
         Tire hankookTire = car1.getHankookTire();
         KiaTire kiaTire = (KiaTire) car2.getKiaTire();
 
