438ddc6c19deecd6de6d2c1b2d7bb578397128f1
Kidongg <dlrlehd7@daum.net>
2023-06-26 12:32:47 +0900

    second homework

--- src/Week_03/homework_1/Calculator.java
+++ src/Week_03/homework_1/Calculator.java
@@ -7,6 +7,7 @@ public class Calculator {
             case "빼기": return firstNumber-secondNumber;
             case "나누기": return firstNumber/secondNumber;
             case "곱하기": return firstNumber*secondNumber;
+            case "나머지": return firstNumber%secondNumber;
 
             default: return 0;
         }
--- src/Week_03/homework_1/Main.java
+++ src/Week_03/homework_1/Main.java
@@ -13,8 +13,11 @@ public class Main {
         double divisionResult = calculator.calculate("나누기", 3, 3);
         System.out.println(divisionResult);  // 1.0
 
-        double MultiplicationResult = calculator.calculate("곱하기", 3, 3);
-        System.out.println(MultiplicationResult);  // 9.0
+        double multiplicationResult = calculator.calculate("곱하기", 3, 3);
+        System.out.println(multiplicationResult);  // 9.0
+
+        double remainderResult = calculator.calculate("나머지", 3, 3);
+        System.out.println(remainderResult);  // 0.0
 
         double ErrorResult = calculator.calculate("인사하기", 3, 3);
         System.out.println(ErrorResult);  // 0.0
